bf32f3b9986e7cdb425ecc69d561fe92c042d343
ATPod <atpod.git@gmail.com>
2015-07-01 22:19:09 +0300

    initial Local Storage service added

--- app/scripts/directives/atpodRcpSearch.js
+++ app/scripts/directives/atpodRcpSearch.js
@@ -29,26 +29,6 @@ angular.module('recipesApp')
                   }).error(function() {
                     console.log('Search failed!');
                   });
-          /*
-          $http(
-                  {method: 'JSONP',
-                   url: 'http://localhost:8983/solr/collection1/select',
-                   params:{'json.wrf': 'JSON_CALLBACK',
-                          'q': self.query,
-                          'wt': 'json'}
-                  })
-                  .success(function(data) {
-                    var docs = data.response.docs;
-                    if(angular.isObject(data.response) && 
-                      !(angular.isUndefined(data.response))){
-                        console.log('search success!', self.query);
-                        $scope.resultsContainer.docs = docs;
-                    } else {
-                    	console.log('Search failed!');
-                    }
-                  }).error(function() {
-                    console.log('Search failed!');
-          });*/
           console.log('scope.results1:', $scope.resultsContainer);//return 
         };
       }
---
+++ app/scripts/services/atpodRcpLSQueries.js
@@ -0,0 +1,5 @@
+(function(){
+	'use strict';
+	// load from ls on app start up
+	// save to ls after each search request
+});
